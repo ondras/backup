567f8129cca8be64915b81f0ebd3aab58ff74773
Ondrej Zara <ondrej.zara@gmail.com>
2019-01-06 12:18:07 +0100

    base

--- backup.sh
+++ backup.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+F=$(readlink "$0")
+BASE=$(dirname "$F")
+
 LOGFILE=/tmp/backup.log
 FULL_AGE="1M"
 MAX_AGE="3M"
 BUCKET=""
 EXTRA=""
 
-. ./backup.secrets  # credentials, ...
-. ./backup.pre   # dump db, ...
+. $BASE/backup.secrets  # credentials, ...
+. $BASE/backup.pre   # dump db, ...
 
 export PASSPHRASE
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
-duplicity --log-file "$LOGFILE" --full-if-older-than $FULL_AGE --include-filelist backup.list --exclude '**' / s3://s3.wasabisys.com/$BUCKET $EXTRA
-duplicity --log-file "$LOGFILE" remove-older-than $MAX_AGE --force s3://s3.wasabisys.com/$BUCKET $EXTRA
+duplicity --log-file "$LOGFILE" --full-if-older-than $FULL_AGE --include-filelist $BASE/backup.list --exclude '**' / s3://s3.wasabisys.com/$BUCKET $EXTRA
+duplicity --log-file "$LOGFILE" remove-older-than $MAX_AGE s3://s3.wasabisys.com/$BUCKET $EXTRA
 
-. ./backup.post  # email logfile, ...
+. $BASE/backup.post  # email logfile, ...
 
 rm "$LOGFILE"
+

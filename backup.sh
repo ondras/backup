44b844aeed8ebb8f70e0b34c6c7f27908996fb41
Ondrej Zara <ondrej.zara@gmail.com>
2019-01-14 09:10:36 +0100

    devnull

--- backup.sh
+++ backup.sh
@@ -16,10 +16,9 @@ export PASSPHRASE
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 
-duplicity --log-file "$LOGFILE" --full-if-older-than $FULL_AGE --include-filelist $BASE/backup.list --exclude '**' / s3://s3.wasabisys.com/$BUCKET $EXTRA
-duplicity --log-file "$LOGFILE" remove-older-than $MAX_AGE s3://s3.wasabisys.com/$BUCKET $EXTRA
+duplicity --log-file "$LOGFILE" --full-if-older-than $FULL_AGE --include-filelist $BASE/backup.list --exclude '**' / s3://s3.wasabisys.com/$BUCKET $EXTRA > /dev/null
+duplicity --log-file "$LOGFILE" remove-older-than $MAX_AGE s3://s3.wasabisys.com/$BUCKET $EXTRA > /dev/null
 
 . $BASE/backup.post  # email logfile, ...
 
 rm "$LOGFILE"
-
